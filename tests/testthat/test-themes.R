afbf2fe52fe8b3d4d57efc235acda87cb514b5b3
ben-lavelle <benjamin.lavelle@justice.gov.uk>
2019-10-28 17:57:24 +0000

    Add a dummy theme for testing

--- tests/testthat/test-themes.R
+++ tests/testthat/test-themes.R
@@ -1,5 +1,9 @@
 context("Theme files")
 
+themecolourspec <- c("accent1", "accent2", "accent3",
+                     "accent4", "accent5", "accent6",
+                     "hlink", "hlinkfol")
+
 themefiles <- list.files(here::here("themes"), pattern = "*.json")
 otherfiles <- setdiff(list.files(here::here("themes")), themefiles)
 
@@ -18,9 +22,6 @@ test_that("all themes load and have metadata", {
 })
 
 test_that("all themes have all accent specified as hex", {
-  themecolourspec <- c("accent1", "accent2", "accent3",
-                       "accent4", "accent5", "accent6",
-                       "hlink", "hlinkfol")
   for (json in themefiles){
     theme <- jsonlite::read_json(here::here("themes", json))
     hexlist <- unlist(lapply(theme$palette, "[[", "hex"))
@@ -30,3 +31,16 @@ test_that("all themes have all accent specified as hex", {
                  label = paste0("Hexadecimal specs of all ", json, " colours"), expected.label = "`\\#[:alphanum:]{6}`")
   }
 })
+
+test_that("dummy theme follows same rules as real themes", {
+    theme <- jsonlite::read_json(here::here("tests", "testthat", "dummy-theme.json"))
+    expect_true(length(theme$name)==1, label="Dummy has a `name` attribute")
+    expect_true(length(theme$updated)==1, label="Dummy has an `updated` attribute")
+    expect_error(as.Date(theme$updated), NA, label="in dummy theme, `updated` attribute is a date")
+    hexlist <- unlist(lapply(theme$palette, "[[", "hex"))
+    expect_setequal(names(theme$palette), themecolourspec)
+    expect_true(length(hexlist)==length(themecolourspec), label = "All colours have a hex")
+    expect_equal(stringr::str_detect(hexlist, "^\\#[0-9a-zA-Z]{6}$"), rep(TRUE, length(themecolourspec)),
+                 label = "Hexadecimal specs of all dummy colours", expected.label = "`\\#[:alphanum:]{6}`")
+})
+
